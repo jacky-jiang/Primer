d3ca25cb06b943d6859a3fad94ab3a12614881c8
jiangyi1 <jiangyi01@babytree-inc.com>
2018-11-17 21:03:21 +0800

    bookstore program

---
+++ ch1/bookstore/b1.cpp
@@ -0,0 +1,20 @@
+/*
+ * b1.cpp
+ *
+ *  Created on: Nov 17, 2018
+ *      Author: jiangyichy
+ */
+
+#include <iostream>
+#include "Sales_item.h"
+
+int main() {
+	Sales_item book;
+
+	std::cin >> book;
+	std::cout << book << std::endl;
+
+	return 0;
+}
+
+
---
+++ ch1/bookstore/b2.cpp
@@ -0,0 +1,20 @@
+/*
+ * b2.cpp
+ *
+ *  Created on: Nov 17, 2018
+ *      Author: jiangyichy
+ */
+
+#include <iostream>
+#include "Sales_item.h"
+
+int main() {
+	Sales_item item1, item2;
+
+	std::cin >> item1 >> item2;
+	std::cout << item1 + item2 << std::endl;
+
+	return 0;
+}
+
+
